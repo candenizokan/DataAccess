cc9a92d2b781645cbd1fa91635320f108a1856d2
Okan Candeniz <candenizokan@gmail.com>
2023-05-07 12:27:22 +0300

    base

---
+++ CrudOperations/BaseItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudOperations
+{
+    internal class BaseItem
+    {
+        public int ID { get; set; }
+
+        private DateTime _createdDate=DateTime.Now;
+
+        public DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set { _createdDate= value; }
+        }
+
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+    }
+}
