c8420b9155275c1b57094533ae0e0d12651f2961
Okan Candeniz <candenizokan@gmail.com>
2023-05-07 13:48:25 +0300

    context-migrations

--- CrudOperations/AppDbContext.cs
+++ CrudOperations/AppDbContext.cs
@@ -9,5 +9,24 @@ namespace CrudOperations
 {
     internal class AppDbContext:DbContext
     {
+
+        //connection string yazıyorum. override edip eziyorum
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GNRM-LAB05-PC15;Database=DBCrudOperation;User Id=sa;Password=123;");
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    //kendi konfigurasyonlarımızı çağırırız.
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+        //tablo oluşturma
+        public DbSet<Category> Categories { get; set; }
+
+        //migration=göç tüm kodları yazdım uygulama tarafında veritabı tarafında bir göç başlatıyorum
+        //add-migration buna isim vermem lazım add-migration one
     }
 }
---
+++ CrudOperations/Migrations/20230507104748_one.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudOperations.Migrations
+{
+    public partial class one : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    DeleteDate = table.Column<DateTime>(nullable: true),
+                    Statu = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
