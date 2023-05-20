ec58b990c85eec38c1e22bbc60f6eb31397dc0c4
Okan Candeniz <candenizokan@gmail.com>
2023-05-20 12:52:12 +0300

    add enum and baseitem

---
+++ LinqToSql/Entities/Abstract/BaseItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSql.Entities.Abstract
+{
+    public abstract class BaseItem<T> //Generic Type nerede t görsem Type
+    {
+        public T Id { get; set; }
+
+        private DateTime _createdDate=DateTime.Now;
+
+        public DateTime CreatedDate
+        {
+            get { return _createdDate=DateTime.Now; }
+            set { _createdDate= value; }
+        }
+
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+        
+    }
+}
