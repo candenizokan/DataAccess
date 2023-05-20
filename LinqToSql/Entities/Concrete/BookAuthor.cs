a653614a4efcd85b440ba7a7dbe8397f766fa242
Okan Candeniz <candenizokan@gmail.com>
2023-05-20 14:05:37 +0300

    bookauthor

--- LinqToSql/Entities/Concrete/BookAuthor.cs
+++ LinqToSql/Entities/Concrete/BookAuthor.cs
@@ -1,6 +1,15 @@
 ﻿namespace LinqToSql.Entities.Concrete
 {
-    public class BookAuthor
+    public class BookAuthor//ck=fk+pk
     {
+        //ara tablolarda bir kere daha sql'in id vermesi istemez. çünkü bu aynı nesneyi defalarca ekleyebilmek demektir.
+
+        // ara tablolarda composite key = foreign key + primary key olması söz konusudurç
+        //iki id nin varlığı eşsiz anahtarı sağlar
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
     }
 }
