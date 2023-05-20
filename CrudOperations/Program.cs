11acca679cb3a5fe1f4852ab0d89c175009c643e
Okan Candeniz <candenizokan@gmail.com>
2023-05-20 10:46:00 +0300

    crud

--- CrudOperations/Program.cs
+++ CrudOperations/Program.cs
@@ -10,6 +10,18 @@ namespace CrudOperations
     {
         static void Main(string[] args)
         {
+
+            /*
+             
+             CRUD=> Crun operations
+
+            Create=> yeni bir nesne oluşturma ve veritabanına ekleme
+            Read=> bir filtreleme, sorgu dahilinde bir / birkaç tane nesneyi veritabanından çekip okumaya çalışmak.
+            Update=> bir filtreleme dahilinde bir yada birkaç nesnenin güncellenmesi
+            Delete=> bir filtre yada sorgu dahilinde ilgili nesne/nesneleri siler
+             
+             
+             */
         }
     }
 }
