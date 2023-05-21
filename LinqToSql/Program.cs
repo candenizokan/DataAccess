648eceefc72089ad47229d98b7421564046d0eb6
Okan Candeniz <candenizokan@gmail.com>
2023-05-21 13:29:22 +0300

    select

--- LinqToSql/Program.cs
+++ LinqToSql/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToSql.Infastructure.Context;
+using AppContext = LinqToSql.Infastructure.Context.AppContext;
 
 namespace LinqToSql
 {
@@ -12,6 +14,17 @@ namespace LinqToSql
         {
 
             //LinqToSql
+
+            using (var db = new AppContext())
+            {
+                //tüm yazarları getirelim
+
+                var list = db.Authors.ToList();
+
+                //SELECT => seçim yapar. ilgili tablodan yalnızca yazdığımız kolonları seçer.
+
+                var list2 = db.Authors.Select(a => new { a.Id, a.Statu, a.FirstName }).ToList();
+            }
         }
     }
 }
