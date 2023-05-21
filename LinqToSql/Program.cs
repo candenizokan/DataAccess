2ffb2c87d8aa976015ce2589a37e6c0ce33571bc
Okan Candeniz <candenizokan@gmail.com>
2023-05-21 13:38:22 +0300

    select

--- LinqToSql/Program.cs
+++ LinqToSql/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToSql.Entities.Concrete;
 using LinqToSql.Infastructure.Context;
 using AppContext = LinqToSql.Infastructure.Context.AppContext;
 
@@ -15,6 +16,7 @@ namespace LinqToSql
 
             //LinqToSql
 
+            #region Select
             using (var db = new AppContext())
             {
                 //tüm yazarları getirelim
@@ -24,7 +26,11 @@ namespace LinqToSql
                 //SELECT => seçim yapar. ilgili tablodan yalnızca yazdığımız kolonları seçer.
 
                 var list2 = db.Authors.Select(a => new { a.Id, a.Statu, a.FirstName }).ToList();
-            }
+
+                //daha küçük bir protatip gibi sunabilirim.
+                var list3 = db.Authors.Select(a => new Calisan { Ad = a.FirstName, Soyad = a.LastName }).ToList();
+            } 
+            #endregion
         }
     }
 }
