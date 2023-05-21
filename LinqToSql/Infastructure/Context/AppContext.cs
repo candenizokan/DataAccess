e99f8ee9cf90971d2b81119c0e9e3e76f091b798
Okan Candeniz <candenizokan@gmail.com>
2023-05-21 11:32:52 +0300

    context

---
+++ LinqToSql/Infastructure/Context/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using LinqToSql.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSql.Infastructure.Context
+{
+    public class AppContext :DbContext
+    {
+        //connectionstring
+        //dbset - oluşacak tablolar
+        // konfigurasyon
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GNRM-LAB05-PC15;Database=LinqToSql;User Id=sa;Password=123;");
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Gendre> Gendres { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+    }
+}
