55ca7b56c7438d4abae75beca30c803fbb649f09
Okan Candeniz <candenizokan@gmail.com>
2023-05-21 10:30:05 +0300

    add efcore sql server

---
+++ LinqToSql/Infastructure/SeedData/SeedGenre.cs
@@ -0,0 +1,19 @@
+﻿using LinqToSql.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSql.Infastructure.SeedData
+{
+    public class SeedGenre : IEntityTypeConfiguration<Gendre>
+    {
+        public void Configure(EntityTypeBuilder<Gendre> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
